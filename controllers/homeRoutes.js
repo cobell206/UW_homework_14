cab21fc4b3eaeb915a152e88482372667a6bcdf3
Colin Bell <cobell206@gmail.com>
2021-05-02 16:44:15 -0700

    changed profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,8 +32,7 @@ router.get('/', withAuth, async (req, res) => {
             logged_in: req.session.logged_in,
         })
     } catch (error) {
-        res.render('login')
-        // res.status(500).json(error)
+        res.status(500).json(error)
         
     }
 })
